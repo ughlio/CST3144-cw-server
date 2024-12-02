8ba8db99570db8579af38eb1dc29f7d466ad7ca5
Fahim Shirzad <fahimshirzaduk@gmail.com>
2024-12-02 05:49:40 +0000

    neater looking middleware logging, using morgan(short)

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static('public'));
-app.use(morgan('combined'));
+app.use(morgan('short'));
 
 // Logger middleware
 app.use((req, res, next) => {
